db33ee762301d82ee8e7642e4743a67599af7e7c
Art Nadaryan <printfrc@gmail.com>
2017-01-30 14:56:17 -0800

    Initial commit

---
+++ app/src/main/java/com/nx/printart/builditbigger/MainFragment.java
@@ -0,0 +1,36 @@
+package com.nx.printart.builditbigger;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button getJokeButton = (Button) view.findViewById(R.id.get_joke_button);
+        //call android library to show a joke
+
+
+        AdView mAdView = (AdView) view.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        return view;
+    }
+}
